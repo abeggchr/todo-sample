118cc3c8347ee7707867740af03320a2c83d5f91
Tobias Zürcher <tobias.zuercher@gmail.com>
2015-02-26 22:20:26 +0100

    add testproject

---
+++ ToDoSample.Tests/SampeTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ToDoSample.Tests
+{
+    [TestFixture]
+    public class SampeTests
+    {
+        [Test]
+        public void Todo_Feature_1_Works()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
