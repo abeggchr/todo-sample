08686466c3a4a0a73ea49e76a644e10b4ce93277
Tobias Zürcher <tobias.zuercher@gmail.com>
2015-02-27 10:17:08 +0100

    add stylecop

---
+++ tools/RegexCounter.ps1
@@ -0,0 +1,14 @@
+﻿param(
+  [string]$path,
+  [string]$regex,
+  [string]$name
+)
+
+$counter = 0
+
+Get-ChildItem $path -Recurse -Filter "*.cs" | Get-Content | % { 
+    $result = [regex]::Matches($_, $regex, "IgnoreCase") 
+    $counter += $result.Count
+}
+
+Write-Host "##teamcity[buildStatisticValue key='$name' value='$counter']"
